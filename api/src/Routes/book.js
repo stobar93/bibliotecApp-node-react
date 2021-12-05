d42a66b4ce1876d2d00182ea3b9e458697bb79b4
stobar93 <sebastiantobarquintero@gmail.com>
2021-12-04 23:00:12 -0500

    PUT /book availability update, OK

--- api/src/Routes/book.js
+++ api/src/Routes/book.js
@@ -1,13 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 const {User, Book} = require('../db');
-const {createBook} = require('../utils/book')
+const {createBook, updateBookAvailability} = require('../utils/book')
 
 router.get('/', (req, res)=>{res.status(200).send('Book router')})
 
 router.post('/', createBook)
 
-router.put('/', (req, res)=>{res.status(200).send('Book router')})
+router.put('/', updateBookAvailability)
 
 router.delete('/', (req, res)=>{res.status(200).send('Book router')})
 
--- api/src/utils/book.js
+++ api/src/utils/book.js
@@ -1,11 +1,11 @@
 const {Book} = require('../db')
 const { conn, Op } = require("../db.js");
+
 const createBook = (req, res, next)=>{
     
         const {title, author, subject, year, pages} = req.body;
         
-        
-            Book.findOrCreate({
+        Book.findOrCreate({
                 where:{title: {
                     [Op.iLike]: title
                 }},
@@ -14,12 +14,21 @@ const createBook = (req, res, next)=>{
                 const [book, createdBook] = data;
                 
                 createdBook ? res.status(200).send(book) : next({status:409, message: "The book is already registered. Only one unit of each book is allowed"})
-            }).catch((e)=>{next({...e, message: `There was an error (${e.message}), please try again.`})})
-        
-
-
-
-    
+            }).catch((e)=>{next({...e, message: `There was an error (${e.message}), please try again.`})})   
 }
 
-module.exports = {createBook}
+const updateBookAvailability = (req, res, next)=>{
+    const {id, available} = req.query
+
+    Book.findByPk(id)
+    .then((instance)=> {
+        if(instance.available !== eval(available)){instance.available = available}
+        else {throw new Error(`This book's availability status is already ${available}`)} 
+        
+        return instance.save()
+    })
+    .then((updatedInstance)=> res.status(200).send({updatedInstance, message: "Changes saved successfully"}))
+    .catch((e)=>next({status: 400, message: `There was an error (${e.message}), please try again.`}))
+}
+
+module.exports = {createBook, updateBookAvailability}
