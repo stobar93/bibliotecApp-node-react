3b75b6284912734d8bf0c4408b6a5e222dd72622
stobar93 <sebastiantobarquintero@gmail.com>
2021-12-04 22:26:47 -0500

    Load Book mockup on server start

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const server = require('./src/app.js');
 const { conn } = require("./src/db.js");
-
-
+const books = require('./src/mock/books')
+const {Book} = require('./src/db.js')
 
 conn.sync({ force: true }).then(()=>{
     server.listen(3001, ()=>{
+
         console.log(`%s listening at 3001`)
+        console.log(Book)
+        Book.bulkCreate(books)
+        .then(()=>console.log("Books loaded"))
+        .catch((e)=>console.log(`There was an error loading Books: ${e.message}`))
     })
 }).catch(e=>console.log(e))
 
