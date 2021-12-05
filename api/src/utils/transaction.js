8aa56d4a7910007014d055959f54a8c28c282445
stobar93 <sebastiantobarquintero@gmail.com>
2021-12-05 18:14:18 -0500

    GET /transaction OK

--- api/src/utils/transaction.js
+++ api/src/utils/transaction.js
@@ -19,34 +19,43 @@ const createTransaction = (req, res, next)=>{
                     //Si es prestamo y el libro tiene estado available: false, informo que no esta disponible para prestamo
                     !book.available && (()=>{throw new Error(`Book id: ${bookId} is not available`)}) 
                 } else if(type === 'return'){
-                    //Si es retorno de libro pero aparece como disponible, no se puede retornar.
+                    //Si es retorno de libro pero aparece como disponible en la base de datos, no se puede retornar.
                     //Maximo un ejemplar de cada libro
                     book.available && (()=>{throw new Error(`Book id: ${bookId} was already returned`)})
                 }
     
+                //Valido si existe una transaccion abierta de ese libro (Es decir, si esta en prestamo)
                 return Transaction.findOne({
                     where: {bookId, status: 'open'}
                 }) 
             } else {throw new Error(`Book id: ${bookId} not found`)}
         })
         .then((openTransaction)=>{
-            if(openTransaction){
+            if(openTransaction){//Si existe la orden abierta
+                
+                //Si es prestamo, error: no se puede prestar
                 type === 'lend' && (()=>{throw new Error(`Cannot lend book : ${bookId}. There is one open transaction related to it`)})()
                 
+                //Si es retorno
                 if(type === 'return'){
-                    if(openTransaction.userId === userId){
+                    if(openTransaction.userId === userId){//Valida que esa transaccion abierta corresponda al usuario
+                        //Actualiza el estado de la orden y guarda cambios
                         openTransaction.status = 'closed',
                         openTransaction.save()
                     } else {throw new Error(`Cannot return book : ${bookId}. Request userId does not match with transaction userId`)}
                 } 
                     
-            } else{
-                type === 'lend' && Transaction.create({userId, bookId, status: 'open'})
+            } else{//Si la orden abierta no existe
+                //Si es prestamo, crea la nueva transaccion con estado 'open'
+                type === 'lend' && Transaction.create({userId, bookId, status: 'open'}) 
+                //Si es retorno, error: no hay ordenes abiertas de ese libro
                 type === 'return' && (()=>{throw new Error(`Cannot return book. There are no open transactions related to user: ${userId} and book: ${bookId}`)})()
             }
         })
-        .then(()=>Book.findByPk(bookId))
+        .then(()=>Book.findByPk(bookId)) //Busco e instancio el libro nuevamente por PK
         .then((book)=>{
+            //modifico propiedad available y guardo cambios
+            //Si la transaccion era prestamo: available=false
             book.available = type === 'lend' ? false : true
             book.save()}
             )
@@ -56,15 +65,20 @@ const createTransaction = (req, res, next)=>{
 }
 
 const readTransaction = (req, res, next)=>{
-    // const {id} = req.query;
+    const whereObj = {...req.query}
 
-    // id ? Book.findByPk(id).then((instance)=>{
-    //     if(instance) {res.status(200).send(instance)}
-    //     else {throw new Error(`Book id: ${id} not found`)}
-    // }).catch((e)=>next({status: 404, message: `There was an error (${e.message})`}))
-    // :
-    // Book.findAll().then((data)=>res.status(200).send(data))
-    // .catch((e)=>next(e))
+    if(whereObj){
+        
+        Object.keys(whereObj).forEach((key)=> !whereObj[key] && delete whereObj[key])
+
+        Transaction.findAll({
+            where: {...whereObj},
+        }).then((transaction)=>res.status(200).send(transaction))
+    }else{
+        Transaction.findAll()
+        .then((data)=>res.status(200).send(data))
+    }
+    
 }
 
 const updateTransaction = (req, res, next)=>{
