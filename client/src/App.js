c76d6411450fe966580c3695632ac255033d833f
stobar93 <sebastiantobarquintero@gmail.com>
2021-12-05 22:26:49 -0500

    Routes working. Now on react-router-dom ^6.0.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Outlet } from "react-router-dom";
 import NavBar from './Components/NavBar/NavBar';
-import Books from './Components/Books/Books';
-import Users from './Components/Users/Users';
-import Transactions from './Components/Transactions/Transactions';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<NavBar />} />
-        <Route exact path="/books" element={<Books />} />
-        <Route exact path="/users" element={<Users />} />
-        <Route exact path="/transactions" element={<Transactions />} />
-      </Routes>
+      <NavBar />
+      <Outlet />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import './index.css';
 import App from './App';
+
+import Books from './Components/Books/Books';
+import Users from './Components/Users/Users';
+import Transactions from './Components/Transactions/Transactions';
 import reportWebVitals from './reportWebVitals';
 dotenv.config();
 
@@ -13,7 +18,13 @@ axios.defaults.baseURL = "http://localhost:3001";
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+    <Routes>
+        <Route path="/" element={<App />}>
+          <Route exact path="/books" element={<Books />} />
+          <Route exact path="/users" element={<Users />} />
+          <Route exact path="/transactions" element={<Transactions />} />
+        </Route>
+      </Routes>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
